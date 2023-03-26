9870fb51217663b022eaf42b98e5584b4eee2ac6
Dorothy <dorothychauu@gmail.com>
2023-03-26 12:13:45 -0400

    timeline added to upcoming events

--- client/src/components/Events/AllEvents.jsx
+++ client/src/components/Events/AllEvents.jsx
@@ -35,56 +35,9 @@ export default function UpcomingEvents() {
   <>
     <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
       <img 
-        src="https://static.wikia.nocookie.net/powerpuff/images/e/e8/Powerpuff-girls-mojo-jojo-brother-1440611559.gif/revision/latest/scale-to-width-down/500?cb=20160617183022" 
+        src="https://cdn.dribbble.com/users/1808107/screenshots/11460419/media/0631ec21d8c5b09e3c8d1a51154152ea.gif" 
         alt="" className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center" />
-      <svg
-        viewBox="0 0 1097 845"
-        aria-hidden="true"
-        className="hidden transform-gpu blur-3xl sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:w-[68.5625rem]"
-      >
-        <path
-          fill="url(#10724532-9d81-43d2-bb94-866e98dd6e42)"
-          fillOpacity=".2"
-          d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z"
-        />
-        <defs>
-          <linearGradient
-            id="10724532-9d81-43d2-bb94-866e98dd6e42"
-            x1="1097.04"
-            x2="-141.165"
-            y1=".22"
-            y2="363.075"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#322c9f" />
-            <stop offset={1} stopColor="#red" />
-          </linearGradient>
-        </defs>
-      </svg>
-      <svg
-        viewBox="0 0 1097 845"
-        aria-hidden="true"
-        className="absolute left-1/2 -top-52 -z-10 w-[68.5625rem] -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
-      >
-        <path
-          fill="url(#8ddc7edb-8983-4cd7-bccb-79ad21097d70)"
-          fillOpacity=".2"
-          d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z"
-        />
-        <defs>
-          <linearGradient
-            id="8ddc7edb-8983-4cd7-bccb-79ad21097d70"
-            x1="1097.04"
-            x2="-141.165"
-            y1=".22"
-            y2="363.075"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#776FFF" />
-            <stop offset={1} stopColor="#FF4694" />
-          </linearGradient>
-        </defs>
-      </svg>
+
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Upcoming Events</h2>
@@ -94,16 +47,16 @@ export default function UpcomingEvents() {
         </div>
       </div>
     </div>
-    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">    
-      <nav className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 xl:gap-x-8">
-        {events.map((event) => (
-          <EventCard 
-            event={event}
-            handleClick={handleClick}       
-          />
-        ))}
-      </nav>
-    </div>
+
+    <ol class="items-center sm:flex">
+      {events.map((event) => (
+        <EventCard 
+          event={event}
+          handleClick={handleClick}       
+        />
+      ))}
+    </ol>
+
     <EventDetailPage event={clicked}/> 
   </>
   )
--- client/src/components/Events/EventCard.jsx
+++ client/src/components/Events/EventCard.jsx
@@ -5,30 +5,31 @@ import EventDetailPage from './EventDetailPage'
 
 
 export default function EventCard({ handleClick, event }) {
-
+//  need to add conditionals to only show upcoming 4 events for dates after today
     return (
-    <div onClick={()=>{handleClick(event)}} key={event.id} className="group relative">
-        <div 
-            className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+    <div onClick={()=>{handleClick(event)}} key={event.id} classNameName="group relative">
+        <li className="relative mb-6 sm:mb-0">
+        <div className="flex items-center">
+            <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+            </div>
+            <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+        </div>
+        
+        <div className="mt-3 sm:pr-8">
+            
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">#{event.event_number} {event.name}</h3>
+            <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{event.date} @ {event.location}</time>
             <img src=
                 {event.poster_img ? 
                     event.poster_img : 
-                    "https://static.wikia.nocookie.net/powerpuff/images/e/e8/Powerpuff-girls-mojo-jojo-brother-1440611559.gif/revision/latest/scale-to-width-down/500?cb=20160617183022"
+                    "https://he-s3.s3.amazonaws.com/media/uploads/d70c703.jpg"
                 }                
                 alt="event promo"
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                classNameName="h-full w-full object-cover object-center lg:h-full lg:w-full"
             />
-            </div>
-        <div className="mt-4 flex justify-between">
-            <div>
-                <h3 className="text-sm font-medium text-black-700">
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    #{event.event_number} {event.name}
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">
-                    {event.date} @ {event.location}
-                </p>
-            </div>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
         </div>
+        </li>
     </div>
 )}
