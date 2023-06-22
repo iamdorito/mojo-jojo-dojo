1e23e6aa601b20ced5ebdafeed1c8f10335ed4f2
Dorothy <dorothychauu@gmail.com>
2023-06-21 23:45:41 -0400

    latest updates

--- client/src/components/Announcements.jsx
+++ client/src/components/Announcements.jsx
@@ -1,21 +1,44 @@
-
 const posts = [
   {
     id: 1,
-    title: 'Road to ONE',
-    href: '#',
+    title: 'BREAKING NEWS: New Matchup',
     description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
+      'Within the heavyweight division, the new matchup is making headlines is Seb vs Lil Arturo.',
+    date: 'March 1, 2023',
+    category: { title: 'Fights'},
     author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
+      name: 'Miss Sara Bellum',
+      role: "Secretary of the Mayor",
       imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        'https://64.media.tumblr.com/480e7fd268d6366bbe0c92752725dd20/tumblr_pvmi7xAQYh1wjgk35o1_1280.png',
     },
+  },{
+    id: 2,
+    title: 'BREAKING NEWS: New Event',
+    description:
+      'Adding a new event to the line up to see who Seb will be fighting',
+    date: 'March 6, 2023',
+    category: { title: 'Events'},
+    author: {
+      name: 'Miss Sara Bellum',
+      role: "Secretary of the Mayor",
+      imageUrl:
+        'https://64.media.tumblr.com/480e7fd268d6366bbe0c92752725dd20/tumblr_pvmi7xAQYh1wjgk35o1_1280.png',
+    },
+  },{
+    id: 3,
+    title: 'BREAKING NEWS: New Fight',
+    description:
+      "Cross division fight coming up between Ella Possible and SebBot. You won't want to miss this!",
+    date: 'March 10, 2023',
+    category: { title: 'Fights'},
+    author: {
+      name: 'Miss Sara Bellum',
+      role: "Secretary of the Mayor",
+      imageUrl:
+        'https://64.media.tumblr.com/480e7fd268d6366bbe0c92752725dd20/tumblr_pvmi7xAQYh1wjgk35o1_1280.png',
+    },
+    
   },
   // More posts...
 ]
@@ -25,13 +48,14 @@ export default function Announcements() {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Big things are coming</h2>
+          <h2 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Here's the Latest</h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
-            Learn how to grow your business with our expert advice.
+            Mojo Jojo's Dojo is where good and evil are put aside to fight for talent and bragging rights.
           </p>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
+            // console.log(post)
             <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
               <div className="flex items-center gap-x-4 text-xs">
                 <time dateTime={post.datetime} className="text-gray-500">
